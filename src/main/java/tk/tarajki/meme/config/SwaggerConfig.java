4e36b2e729289ce6b28341fcccc5853833ce2ad1
Krzysiek <kbachon97@gmail.com>
2019-06-05 15:32:43 +0200

    update swagger

--- src/main/java/tk/tarajki/meme/config/SwaggerConfig.java
+++ src/main/java/tk/tarajki/meme/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package tk.tarajki.meme.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,10 +20,10 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("tk.tarajki.meme.controllers"))
                 .paths(PathSelectors.any())
-
                 .build();
     }
 
